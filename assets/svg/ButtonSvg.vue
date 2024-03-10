d03ab83cd489dc3a80d50c426549cf0b54f35818
SamanSah <sahraei.saman91@gmail.com>
2024-03-11 01:40:04 +0330

    fix svg button

--- assets/svg/ButtonSvg.vue
+++ assets/svg/ButtonSvg.vue
@@ -52,13 +52,9 @@
         d="M0,43.00005 L5.028,43.00005 L12.24,43.00005 C16.526,43.00005 20,39.58105 20,35.36365 L20,16.85855 C20,14.59295 18.978,12.44425 17.209,10.99335 L7.187,2.77111 C5.792,1.62675 4.034,1 2.217,1 L0,1"
       />
     </svg>
-
-
-
 </template>
 
 <script setup>
-
 defineProps({
   color: {
     type: String,
@@ -66,12 +62,4 @@ defineProps({
   },
 });
 
-const svgClass = computed(() => {
-  return {
-    absolute: true,
-    'top-0': true,
-    'left-[1.3125rem]': true,
-    'w-[calc(100%-2.625rem)]': true,
-  };
-});
 </script>
