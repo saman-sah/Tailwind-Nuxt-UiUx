61fa782ed80791f23935f5e7883e15d4986becd7
SamanSah <sahraei.saman91@gmail.com>
2024-03-14 01:18:05 +0330

    complete collaboration component

--- components/collaboration/index.vue
+++ components/collaboration/index.vue
@@ -33,14 +33,71 @@
             </p>
           </li>
         </ul>
+
+        <Button>
+          Tri it now
+        </Button>
+
       </div>
+
+      <div class="lg:ml-auto xl:w-[38rem] mt-4">
+        <p class="body-2 mb-8 text-n-4 md:mb-16 lg:mb-32 lg:w-[22rem] lg:mx-auto">
+          {{ collabText }}
+        </p>
+
+        <div class="mt-4 relative left-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale:75 md:scale-100">
+          <div class="flex w-60 aspect-square m-auto border border-n-6 rounded-full">
+            <div class="w-[6rem] aspect-square m-auto p-[0.2rem] bg-conic-gradient rounded-full">
+              <div class="flex items-center justify-center w-full h-full bg-n-8 rounded-full">
+                <img 
+                  :src="brainwaveSymbol" 
+                  alt="brainwave"
+                  width="48px"
+                  height="48px"
+                >
+              </div>
+            </div>
+          </div>
+          <ul>
+            <li
+              v-for="(app, index) in collabApps"
+              :key="app.id"
+              :class="[
+                `rotate-${index*45}`,
+                'absolute top-0 left-1/2 h-1/2 -ml-[1.6rem] origin-bottom'
+              ]"
+            >
+              <div :class="[
+                `-rotate-${index*45}`,
+                'relative -top-[1.6rem] flex w-[3.2rem] h-[3.2rem] bg-n-7 border border-n-1/5 rounded-xl'
+              ]">
+                <img 
+                  :src="app.icon" 
+                  :alt="app.title"
+                  class="m-auto"
+                  :width="app.width"
+                  :height="app.height"
+                >
+              </div>
+            </li>
+          </ul>
+          <LeftCurve />
+          <RightCurve />
+        </div>
+      </div>
+
     </div>
   </SectionBase>
 </template>
 
 <script setup>
-import { check } from '~/assets'
-import { collabContent } from '~/utils/constant'
+import { check, brainwaveSymbol } from '~/assets'
+import { 
+  collabContent, 
+  collabText,
+  collabApps
+} from '~/utils/constant'
 
-console.log('collabContent', collabContent);
+import LeftCurve from '../design/collaboration/LeftCurve.vue'
+import  RightCurve from '../design/collaboration/RightCurve.vue'
 </script>
---
+++ components/design/collaboration/RightCurve.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="hidden absolute top-1/2 left-full w-[10.125rem] -mt-1 ml-10 pointer-events-none xl:block">
+      <img 
+        :src="curve2" 
+        width="162px" 
+        height="76px" 
+        alt="Curve 2"
+      />
+    </div>
+</template>
+
+<script setup>
+import { curve2 } from "~/assets"
+</script>
